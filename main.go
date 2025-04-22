5ecd2599b8792bb4aff1d59b14f40e4fb311a325
christianrm0821 <christianrm0821@gmail.com>
2025-04-22 12:34:46 -0700

    added the new /healthz and /app endpoint tomultiplexer

--- main.go
+++ main.go
@@ -8,13 +8,31 @@ import (
 func main() {
 	//making a newserveMux
 	const port = ":8080"
+
+	//mux or multiplexer
+	//it is a request router
+	// it gets incoming http requests and decides which handler function should process the request
+	//maps url patterns to handler functions
 	serveMux := http.NewServeMux()
 
-	//Handle(function): defines a specific url path on the server, associates path with handler
-	//handler: responsible for processing incoming http requests and returning appropriate responses
-	//fileserver: built in handler using http library, serves static files from specific directories on computer,
-	//needs to know what directory to serve files from(used "." since that is current directory)
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	//handlefunc register handlers with serveMux
+	//takes in the "/healthz" endpoint
+	//takes in a function with the signature "func(http.ResponseWriter, *http.Request)"
+	//It automatically converts your function to a handler interface
+	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	//registers handler with serveMux
+	//takes url path with an object with the method "ServeHTTP(http.ResponseWriter, *http.Request)"
+	//used for pre-built handlers or custom handler type
+	//want to use this over a handle in more complex situations such as with the fileserver handler or using miiddleware like stripPrefix
+	//Strip prefix takes away the prefix "/app" from the handler
+	//FileServer is a built in handler, automatically handles file serving, content types, and directory listings
+	//FileServer serves static content
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 
 	//making the server struct
 	myServer := &http.Server{
@@ -23,9 +41,9 @@ func main() {
 	}
 
 	//start an http server with the port and handler we created above/ handles any errors
+	log.Println("Starting server on port #", port)
 	err := myServer.ListenAndServe()
 	if err != http.ErrServerClosed {
-		log.Printf("Error: %v\n", err)
 		log.Fatal("server error: ", err)
 	}
 
